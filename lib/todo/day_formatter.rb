682bc1b1a2c41add072cb54bef237dc01ccad3a6
Brandon Duff <brandonduff17@gmail.com>
2017-08-18 16:25:43 -0600

    Todo day reports current day if no current day file exists

--- lib/todo/day_formatter.rb
+++ lib/todo/day_formatter.rb
@@ -3,7 +3,11 @@ require 'date'
 module Todo
   class DayFormatter
     def self.format(date_string)
-      date_string == "today" ? Date.today.strftime("%d-%m-%Y") : date_string
+      if date_string == "today" || date_string == ""
+        Date.today.strftime("%d-%m-%Y")
+      else
+        date_string
+      end
     end
   end
 end
