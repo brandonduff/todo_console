0177696dca224b46da6357d25d28faab627d6a2d
Brandon Duff <brandonduff17@gmail.com>
2017-10-20 23:39:16 -0600

    Add test for more listing existing todo, do cleanup

--- test/todo/use_cases/list_todos_test.rb
+++ test/todo/use_cases/list_todos_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
+require 'fileutils'
 
 module Todo
   module UseCases
     class ListTodosTest < Minitest::Test
 
       def setup
+        @original_home = ENV['HOME']
         ENV['HOME'] = 'tmp'
         current_day = Date.parse('10-03-1993')
-        current_day_file_name = 'tmp/current_day.txt'
+        current_day_file_name = 'tmp/.current_day.txt'
         @todo_file_name = 'tmp/todos/10-03-1993.txt'
         Dir.mkdir('tmp') unless Dir.exist?('tmp')
         current_day = File.open(current_day_file_name, 'a')
         current_day.puts('10-03-1993')
+        current_day.close
         Dir.mkdir('tmp/todos') unless Dir.exist?('tmp/todos')
+      end
+
+      def teardown
+        ENV['HOME'] = @original_home
+        FileUtils.rm_rf('tmp')
       end
 
       def test_list_with_no_current_todos_returns_empty_list
@@ -26,6 +34,8 @@ module Todo
         todo_file.puts('hello')
         todo_file.close
 
+        todos = ListTodos.new({}).perform
+        assert_equal(['hello'], todos)
       end
     end
   end
