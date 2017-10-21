5b63298c7930bacb0fde57b160f280f82f1105e2
Brandon Duff <brandonduff17@gmail.com>
2017-10-20 23:12:26 -0600

    Don't stub in tests for use case
    - I think I want these to be more "real." Then they can be tested more
      thoroughly and do lest testing through the communication medium (cli/web)

--- test/todo/use_cases/list_todos_test.rb
+++ test/todo/use_cases/list_todos_test.rb
@@ -3,17 +3,29 @@ require 'test_helper'
 module Todo
   module UseCases
     class ListTodosTest < Minitest::Test
-      def test_list_with_no_current_todos_returns_empty_list
-        env_helper = double
-        reader = double
-        current_day = '1-1-2003'
-        allow(EnvHelper).to receive(:new).and_return(env_helper)
-        allow(Reader).to receive(:new).with(env_helper).and_return(reader)
-        allow(reader).to receive(:current_day).and_return(current_day)
 
+      def setup
+        ENV['HOME'] = 'tmp'
+        current_day = Date.parse('10-03-1993')
+        current_day_file_name = 'tmp/current_day.txt'
+        @todo_file_name = 'tmp/todos/10-03-1993.txt'
+        Dir.mkdir('tmp') unless Dir.exist?('tmp')
+        current_day = File.open(current_day_file_name, 'a')
+        current_day.puts('10-03-1993')
+        Dir.mkdir('tmp/todos') unless Dir.exist?('tmp/todos')
+      end
+
+      def test_list_with_no_current_todos_returns_empty_list
         todos = ListTodos.new({}).perform
 
         assert_equal([], todos)
+      end
+
+      def test_list_with_one_todo_returns_the_todo
+        todo_file = File.open(@todo_file_name, 'w+')
+        todo_file.puts('hello')
+        todo_file.close
+
       end
     end
   end
