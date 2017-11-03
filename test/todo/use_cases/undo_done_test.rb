c493724e02b60a66593d987ee903c6d9d730bbac
Brandon Duff <brandonduff17@gmail.com>
2017-11-02 21:54:39 -0600

    Stub out undo done todo use case stuff

---
+++ test/todo/use_cases/undo_done_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+require 'fileutils'
+
+module Todo
+  module UseCases
+    class UndoDoneTodoTest < Minitest::Test
+
+      # TODO: Implement this. Let's start off by not duplicating the logic to setup the file system for these
+      # use-case tests.
+
+
+    end
+  end
+end
