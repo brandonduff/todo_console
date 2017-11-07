27ce9a3e352ec650281717234be3f78549ca064d
Brandon Duff <brandonduff17@gmail.com>
2017-11-06 22:51:38 -0700

    Implement returning of done todo
    - Add pended test for saving todo list

--- lib/todo/use_cases/undo_done.rb
+++ lib/todo/use_cases/undo_done.rb
@@ -3,13 +3,21 @@ module Todo
     class UndoDone
 
       def perform
-        TaskListFetcher.new(Date.parse(read_current_day)).undo
+        present(TaskListFetcher.new(reader).tasks_for_day(read_current_day).done)
       end
 
       def read_current_day
-        Reader.new(EnvHelper.new).current_day
+        reader.current_day
       end
 
+      private
+
+      def present(todo)
+        todo.description
+      end
+      def reader
+        Reader.new(EnvHelper.new)
+      end
     end
   end
 end
--- test/todo/use_cases/undo_done_test.rb
+++ test/todo/use_cases/undo_done_test.rb
@@ -4,11 +4,14 @@ module Todo
   module UseCases
     class UndoDoneTest < Base
       def test_undo_returns_undone_todo
-        skip
         build_todo_file(todo_file_for('10-03-1993'))
         task_list = build_task_list([{ description: 'hello' }, { description: 'goodbye' }])
         save_todo_file(task_list)
-        assert_equal('goodbye', UndoDone.new.perform)
+        assert_equal('✓ hello', UndoDone.new.perform)
+      end
+
+      def test_undo_saves_state_of_todos
+        skip
       end
 
       private
