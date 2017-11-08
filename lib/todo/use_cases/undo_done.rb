e1c39762c76f823c47acc13d8a13f250dbb33bc4
Brandon Duff <brandonduff17@gmail.com>
2017-11-07 22:06:51 -0700

    Undo use case will save todos

--- lib/todo/use_cases/undo_done.rb
+++ lib/todo/use_cases/undo_done.rb
@@ -3,20 +3,34 @@ module Todo
     class UndoDone
 
       def perform
-        present(TaskListFetcher.new(reader).tasks_for_day(read_current_day).done)
+        task_list = TaskListFetcher.new(reader).tasks_for_day(read_current_day)
+        done_task = task_list.done
+        write(task_list)
+        present(done_task)
       end
+
+      private
 
       def read_current_day
         reader.current_day
       end
 
-      private
+      def write(task_list)
+        todo_file = File.open(env_helper.todo_file_for_day(reader.current_day), 'a')
+        Writer.for(task_list).write_to(todo_file)
+        todo_file.close
+      end
 
       def present(todo)
         todo.description
       end
+
       def reader
-        Reader.new(EnvHelper.new)
+        Reader.new(env_helper)
+      end
+
+      def env_helper
+        EnvHelper.new
       end
     end
   end
--- test/todo/use_cases/undo_done_test.rb
+++ test/todo/use_cases/undo_done_test.rb
@@ -3,15 +3,20 @@ require_relative 'base'
 module Todo
   module UseCases
     class UndoDoneTest < Base
-      def test_undo_returns_undone_todo
+      def setup
+        super
         build_todo_file(todo_file_for('10-03-1993'))
         task_list = build_task_list([{ description: 'hello' }, { description: 'goodbye' }])
         save_todo_file(task_list)
-        assert_equal('✓ hello', UndoDone.new.perform)
+      end
+
+      def test_undo_returns_undone_todo
+       assert_equal('✓ hello', UndoDone.new.perform)
       end
 
       def test_undo_saves_state_of_todos
-        skip
+        UndoDone.new.perform
+        assert_equal("✓ hello\ngoodbye\n", File.read(todo_file_for('10-03-1993')))
       end
 
       private
